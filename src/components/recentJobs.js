10b12f8e82e175e2076da317693c7f51f8ac976c
tadiwa <pfachitadiwanashedavid@gmail.com>
2024-01-06 23:21:00 +0200

    recent jobs section done

--- src/components/recentJobs.js
+++ src/components/recentJobs.js
@@ -17,6 +17,7 @@ import { Fragment } from 'react'
 const items = [
     { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote', offer: '$120k &ndash; $140k', closingDate: 'Closing on January 9, 2020' },
     { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote', offer: '$120k &ndash; $140k', closingDate: 'Closing on January 9, 2020' },
+    { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote', offer: '$120k &ndash; $140k', closingDate: 'Closing on January 9, 2020' },
     { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote', offer: '$120k &ndash; $140k', closingDate: 'Closing on January 9, 2020' },
 ]
 
@@ -25,52 +26,57 @@ export default function RecentJobs() {
         return classes.filter(Boolean).join(' ')
     }
     return (
-        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 flex-row">
-            <di className="bg-black h-1080px">            
+        <div className="grid grid-col-10 items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6 flex-row h-dvh grid-cols-12">
+            <di className="grid bg-white h-screen w-full space-y-4 col-start-3 col-span-8 grid-cols-6">
+                <h4 className="col-start-1 col-span-5 h-3">Recently uploaded jobs</h4>
                 {items.map((item) => {
-                return (
-                    <div className="lg:flex lg:items-center lg:justify-between flex-row bg-green-100 ml-10px" >
-                        <div className="min-w-0 flex-1">
-                            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-                                {item.title}
-                            </h2>
-                            <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-                                <div className="mt-2 flex items-center text-sm text-gray-500">
-                                    <BriefcaseIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                    {item.type}
+                    return (
+                        <div className="lg:flex lg:items-center lg:justify-between flex-row  ml-10px h-48 w-full p-16 justify-items-center col-start-1 col-span-8 " >
+                            <div className="min-w-0 flex-1">
+                                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                                    {item.title}
+                                </h2>
+                                <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <BriefcaseIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                        {item.type}
+                                    </div>
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                        {item.location}
+                                    </div>
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                        {item.offer}
+                                    </div>
+                                    <div className="mt-2 flex items-center text-sm text-gray-500">
+                                        <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                        {item.closingDate}
+                                    </div>
+
                                 </div>
-                                <div className="mt-2 flex items-center text-sm text-gray-500">
-                                    <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                    {item.location}
-                                </div>
-                                <div className="mt-2 flex items-center text-sm text-gray-500">
-                                    <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                    {item.offer}
-                                </div>
-                                <div className="mt-2 flex items-center text-sm text-gray-500">
-                                    <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                    {item.closingDate}
-                                </div>
+                                <h4 className="text-xl leading-7 text-gray-900 sm:truncate sm:text-1xl sm:tracking-tight">
+                                    From cloud engineers to developers to security pros, the battle for the best IT talent wages. Heres <br/> what to look for (and what to offer) when hiring for the 15 most in-demand IT jobs for 2024.
+                                </h4>
+                            </div>
+                            <div className="mt-5 flex lg:ml-4 lg:mt-0">
+
+                                <span className="ml-3 hidden sm:block hover:bg-sky-700">
+                                    <a href='https://www.tadiwapfachi.dev' className="hover:text-white hover:border-gray-300 hover:bg-sky-700">
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:border-gray-300 hover:bg-sky-700"
+                                        >
+                                            <LinkIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                            Learn more
+                                        </button>
+                                    </a>
+                                </span>
+
                             </div>
                         </div>
-                        <div className="mt-5 flex lg:ml-4 lg:mt-0 hover:bg-sky-700">
-
-                            <span className="ml-3 hidden sm:block hover:bg-sky-700">
-                                <a href='https://www.tadiwapfachi.dev' className="hover:text-white hover:bg-purple-600 hover:border-gray-300">
-                                    <button
-                                        type="button"
-                                        className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:text-white hover:bg-purple-600 hover:border-gray-300"
-                                    >
-                                        <LinkIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        View more
-                                    </button>
-                                </a>
-                            </span>
-
-                        </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
             </di>
 
 
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,11 +4,42 @@ import React from 'react'
 function SearchBar() {
 
     let locations = ["Harare", "Mutare", "Bulawayo", "Rusape", "VictoriaFalls"];
+    let techJobs = [
+        "Full-Stack Developer",
+        "Front-End Developer",
+        "Back-End Developer",
+        "Mobile App Developer",
+        "DevOps Engineer",
+        "Data Scientist",
+        "Data Analyst",
+        "Machine Learning Engineer",
+        "Cybersecurity Analyst",
+        "Network Engineer",
+        "Cloud Security Engineer",
+        "UX/UI Designer",
+        "Technical Writer",
+        "Quality Assurance Engineer",
+        "Product Manager",
+        "Blockchain Developer",
+        "AR/VR Developer",
+        "AI Ethics Specialist",
+        "IT Project Manager",
+        "IT Support Specialist"
+    ];
 
     return (
         <div className="mt-6 text-lg leading-8 text-gray-600 border border-5px justify-items-center ">
             <form className="text-lg leading-8 text-gray-600 height-100% grid grid-cols-3 gap-4 justify-items-center align-items-center mx-auto" >
-                <input type="search" placeholder="search here" className="p-4 h-100% " />
+                <select className="p-4 h-100%">
+                    <option value="">Select job</option>
+                    {techJobs.map((job) => {
+                        return (
+                            <option key={job} value={job}>
+                                {job}
+                            </option>
+                        );
+                    })}
+                </select>
                 <select className="p-4 h-100%">
                     <option value="">Select location</option>
                     {locations.map((location) => {
