100574380df61a24b4f8d5c2e26370de1852169e
tadiwa <pfachitadiwanashedavid@gmail.com>
2024-01-07 00:33:16 +0200

    x

--- src/components/errors.js
+++ src/components/errors.js
@@ -2,14 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function Example() {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-dvh">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
