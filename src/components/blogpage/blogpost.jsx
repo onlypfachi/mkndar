6066f4634e30a04b42d2416545cbf434264b59f4
tadiwa <pfachitadiwanashedavid@gmail.com>
2024-02-04 08:07:25 +0200

    blogs page now showing blogs

--- src/components/blogpage/blogpost.jsx
+++ src/components/blogpage/blogpost.jsx
@@ -11,19 +11,25 @@ const blog = {
   link: "blog link",
   body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper ante vitae est tristique, vel fermentum arcu cursus. Duis ac sem nec elit tincidunt tincidunt. Quisque a lacinia augue. Phasellus efficitur, justo eu fermentum tempus, ligula augue consequat mauris, a bibendum neque ligula vel justo. Curabitur eu urna vitae nunc congue bibendum. Maecenas id vestibulum urna. In hac habitasse platea dictumst. Sed tristique sapien non est facilisis, at laoreet nunc fringilla. In cursus libero vel quam gravida, vel hendrerit libero temporVestibulum aliquam arcu vel nunc suscipit, vel hendrerit elit euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque ullamcorper metus ut felis imperdiet eleifend. Nam in nulla sit amet lectus fermentum tempus. Nullam vel erat vitae libero dictum facilisis id vel velit. Aenean vehicula, metus eu fermentum tincidunt, libero metus ultrices turpis, ac scelerisque libero felis sit amet libero. Proin ullamcorper mi vel bibendum malesuada. Aenean nec ex ac sapien eleifend bibendum.Pellentesque euismod elit ac augue iaculis, vel cursus urna feugiat. Integer non magna nec sapien finibus congue. Morbi ut eros eu felis feugiat scelerisque nec ac tortor. Vivamus vehicula ipsum eget tellus sodales, eget dictum dui cursus. Integer eu metus vel dolor suscipit luctus ac quis dui. Suspendisse potenti. Sed semper, metus sit amet tincidunt sagittis, sem felis luctus velit, ut tincidunt dui odio a tellus. Curabitur aliquet dui eu sem ullamcorper, ac lacinia dolor dignissim.",
 };
-const date = new Date();
-function Blogpost() {
+
+export default function Blogpost() {
   return (
     <div className="grid items-center bg-white w-full rounded-lg shadow-lg h-64 grid-cols-12 grid-rows-12">
       <span className="grid h-12 grid-cols-12 justify-items-center align-middle w-full row-start-1 row-span-2 col-start-1 col-span-12 overflow-hidden">
         <Avatar className="col-span-1 col-start-1 h-50% bg-white overflow-hidden" />
-        <code className="col-span-3 col-start-2 h-full w-full ">By {blog.author}</code>
-        <code className="col-span-8 col-start-5 h-full w-full">On {blog.date}</code>
+        <code className="col-span-3 col-start-2 h-full w-full ">
+          By {blog.author}
+        </code>
+        <code className="col-span-8 col-start-5 h-full w-full">
+          On {blog.date}
+        </code>
       </span>
-      <h2 className="text-2xl text-left mt-2 ml-5 font-bold row-start-3 row-span-3 col-span-12 align-left whitespace-pre-line">{blog.topic}</h2>
-      <p  className="text-s text-ellipsis overflow-hidden row-start-6 row-span-5 col-span-12 whitespace-pre-line">{blog.body}</p>
+      <h2 className="text-3xl text-left mt-2 ml-5 font-bold row-start-3 row-span-3 col-span-12 align-left whitespace-pre-line">
+        {blog.topic}
+      </h2>
+      <p className="text-sm break-words text-wrap md:text-balance mt-2 text-clip overflow-hidden row-start-6 row-span-5 col-span-12 whitespace-pre-line line-clamp-3">
+        {blog.body}
+      </p>
     </div>
   );
 }
-
-export default Blogpost;
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -5,7 +5,7 @@ export default function BlogsPage() {
   const topics = ["success", "failure", "yellow", " yellow"];
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const color = "#" + randomColor;
-
+console.log(color)
   return (
     <main id="pageTitle" className="grid mt-20 h-36 text-center grid-cols-10">
       <div className="col-start-4 col-span-4">
@@ -14,11 +14,12 @@ export default function BlogsPage() {
           Stay updated with the latest tech insights and trends.
         </p>
       </div>
-      <div className="mt-8 col-start-4 col-span-4" id="topics" >
+      <div className="mt-8 col-start-4 col-span-4" id="topics">
         topics :{" "}
         {topics.map((topic) => {
           return (
             <button
+              // onClick={page.reload()}
               type="button"
               class={`bg-${color}`}
               className={`ml-2 h-8 bg-[${color}]-100 inline-block rounded bg-info px-6 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]`}
@@ -28,8 +29,28 @@ export default function BlogsPage() {
           );
         })}
       </div>
-      <section id="blogs" className="h-dvh  bg-green-900 flex gap-5 col-start-3 col-span-5 mt-6">
-        <Blogpost />
+      <section
+        id="blogs"
+        className="h-dvh flex flex-col gap-5 col-start-3 col-span-6 mt-6"
+      >
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
+        <a href="#pageTitle">
+          <Blogpost />
+        </a>
       </section>
     </main>
   );
