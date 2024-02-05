f79253534a87b50322ee1a1fd8bb89e33e901ab8
tadiwa <pfachitadiwanashedavid@gmail.com>
2024-02-06 00:49:41 +0200

    fixed layout

--- src/components/jobs/MiniJobCard.jsx
+++ src/components/jobs/MiniJobCard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function MiniJobCard() {
   return (
-    <div>MiniJobCards</div>
-  )
+    <div className="bg-grey-200 rounded-lg shadow-lg p-4 h-80 border-zinc-700 border-solid w-48">
+      <h3 className="bold">Junior Software Engineer</h3>
+      <p className="">
+        this is where a short description of the job post are found
+      </p>
+    </div>
+  );
 }
 
-export default MiniJobCard
+export default MiniJobCard;
--- src/pages/jobs.jsx
+++ src/pages/jobs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import img from '../images/bgg.jpg'
-
+// import img from "../images/bgg.jpg";
+import MiniJobCard from "../components/jobs/MiniJobCard";
 
 function jobs() {
   const topics = ["success", "failure", "yellow", " yellow"];
@@ -8,9 +8,11 @@ function jobs() {
   const color = "#" + randomColor;
 
   return (
-    <main>
-      <div id="pageTitle" className="mt-20 h-36 text-center ">
-        <h3 className="h-12 text-4xl font-bold">Discover the Best Blogs</h3>
+    <main className="flex gap-5 flex-col relative">
+      <div id="pageTitle" className="mt-20 h-36 text-center w-full z-99 mb-10">
+        <h3 className="h-12 text-4xl font-bold">
+          Discover Jobs hiring in your city
+        </h3>
         <p className="text-sm not-italic">
           Stay updated with the latest tech insights and trends.
         </p>
@@ -21,57 +23,28 @@ function jobs() {
               <button
                 type="button"
                 class={`bg-${color}`}
-                className={`ml-2 h-8 bg-[${color}]-100 inline-block rounded bg-info px-6 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]`}
+                className="ml-2 h-8 inline-block rounded bg-info px-6 pb-2 pt-2 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
               >
                 {topic}
               </button>
             );
           })}
         </div>
-        <section id='blogs' className="h-dvh  bg-green-900">
-          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img
-                class="rounded-t-lg"
-                src={img}
-                alt=""
-              />
-            </a>
-            <div class="p-5">
-              <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Noteworthy technology acquisitions 2021
-                </h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
-              <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-        </section>
       </div>
+      <section
+        id="jobs"
+        className="grid h-dvh gap-5 bg-white grid-cols-13 w-4/5 m-auto "
+      >
+        <div className="col-start-1 col-span-4 bg-green-500 h-full">
+          <MiniJobCard />
+          <MiniJobCard />
+          <MiniJobCard />
+          <MiniJobCard />
+          <MiniJobCard />
+          <MiniJobCard />
+        </div>
+        <div className="col-start-5 col-span-6 bg-black"></div>
+      </section>
     </main>
   );
 }
