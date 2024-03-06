bd83f755602e443846899de56e8ed8a77d717bf5
tadiwa <pfachitadiwanashedavid@gmail.com>
2024-03-06 20:01:53 +0200

    jobs page done

--- src/pages/jobs.jsx
+++ src/pages/jobs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import JobCard from "../components/jobs/JobCard";
 import FilterBar from "../components/jobs/FilterBar"; // Capitalized import for FilterBar
 
-function Jobs() {
+const Jobs = () => {
   const topics = ["success", "failure", "yellow", "yellow"];
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const color = "#" + randomColor;
@@ -35,10 +35,28 @@ function Jobs() {
       offer: "$120k – $140k",
       closingDate: "Closing on January 9, 2020",
     },
+    {
+      id: 3,
+      title: "User Interface Designer",
+      department: "Design",
+      type: "Full-time",
+      location: "Remote",
+      offer: "$120k – $140k",
+      closingDate: "Closing on January 9, 2020",
+    },
+    {
+      id: 3,
+      title: "User Interface Designer",
+      department: "Design",
+      type: "Full-time",
+      location: "Remote",
+      offer: "$120k – $140k",
+      closingDate: "Closing on January 9, 2020",
+    },
   ];
 
   return (
-    <section className="flex gap-5 flex-col relative m-auto mt-20 ">
+    <section className="flex gap-5 flex-col m-auto mt-20 ">
       <div id="pageTitle" className="h-36 text-center w-full z-99 mb-10">
         <h3 className="h-12 text-4xl font-bold">
           Discover Jobs hiring in your city
@@ -46,10 +64,15 @@ function Jobs() {
         <p className="text-sm not-italic">
           Stay updated with the latest tech insights and trends.
         </p>
-        <FilterBar>
-        </FilterBar>
+        <FilterBar />
       </div>
       <div className="grid gap-6 mt-6 ">
+        {items.map((item) => (
+          <JobCard key={item.id} job={item} />
+        ))}
+      </div>
+      <h1 className="text-center">Related Jobs</h1>
+      <div className="grid gap-4 mt-4 ">
         {items.map((item) => (
           <JobCard key={item.id} job={item} />
         ))}
